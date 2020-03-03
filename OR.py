abea5fb0ff86f832b1f0c2203f5003924d21a4f9
nouarnoorelhouda <nouarnoorelhouda@gmail.com>
2020-03-03 11:40:36 +0100

    Update OR.py

--- OR.py
+++ OR.py
@@ -1,6 +1,8 @@
 import socket
 import select
 
+import args
+
 HEADER_LENGTH = 10
 
 IP = "127.0.0.1"
@@ -80,6 +82,7 @@ while True:
             # Accept new connection
             # That gives us new socket - client socket, connected to this given client only, it's unique for that client
             # The other returned object is ip/port set
+
             client_socket, client_address = server_socket.accept()
 
             # Client should send his name right away, receive it
@@ -88,12 +91,14 @@ while True:
             # If False - client disconnected before he sent his name
             if user is False:
                 continue
-
             # Add accepted socket to select.select() list
-            sockets_list.append(client_socket)
+            for item in args:
+                if item == client_address:
+                    sockets_list.append(client_socket)
+                    clients[client_socket] = user
 
             # Also save username and username header
-            clients[client_socket] = user
+
 
             print('Accepted new connection from {}:{}, username: {}'.format(*client_address, user['data'].decode('utf-8')))
 
